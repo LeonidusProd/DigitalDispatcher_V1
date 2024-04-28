3e82803cde02c147c563094c4f828f0a7d5c037a
leoni <leonidusreg@gmail.com>
2024-04-28 09:37:55 +0300

    Начало фронтенда

--- front/src/pages/LoginPage.vue
+++ front/src/pages/LoginPage.vue
@@ -1,30 +1,32 @@
 <template>
-  <div class="center-container">
-    <div class="logo-box">
-      <img class="logo-img" alt="logo"
-           src="http://localhost:8000/media/assets/%D0%A2%D0%B5%D0%BA%D1%81%D1%82%20%D1%81%D0%BD%D0%B8%D0%B7%D1%83.png">
-    </div>
+  <div class="container">
+    <div class="center-container">
+      <div class="logo-box">
+        <img class="logo-img" alt="logo"
+             src="http://localhost:8000/media/assets/%D0%A2%D0%B5%D0%BA%D1%81%D1%82%20%D1%81%D0%BD%D0%B8%D0%B7%D1%83.png">
+      </div>
 
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Вход в систему</h5>
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Вход в систему</h5>
 
-        <form>
-          <div class="form-group">
-            <label for="username">Имя пользователя</label>
-            <!--            <input type="text" class="form-control" id="username" placeholder="Имя пользователя">-->
-            <my-input type="text" id="username" placeholder="Имя пользователя"></my-input>
-          </div>
+          <form>
+            <div class="form-group">
+              <label for="username">Имя пользователя</label>
+              <!--            <input type="text" class="form-control" id="username" placeholder="Имя пользователя">-->
+              <my-input type="text" id="username" placeholder="Имя пользователя"></my-input>
+            </div>
 
-          <div class="form-group">
-            <label for="password">Пароль</label>
-            <my-input type="password" id="password" placeholder="Пароль"></my-input>
-            <!--            <input type="password" class="form-control" id="password" placeholder="Пароль">-->
-          </div>
+            <div class="form-group">
+              <label for="password">Пароль</label>
+              <my-input type="password" id="password" placeholder="Пароль"></my-input>
+              <!--            <input type="password" class="form-control" id="password" placeholder="Пароль">-->
+            </div>
 
-          <!--          <button type="submit" class="btn btn-primary">Войти</button>-->
-          <my-button>Войти</my-button>
-        </form>
+            <!--          <button type="submit" class="btn btn-primary">Войти</button>-->
+            <my-button>Войти</my-button>
+          </form>
+        </div>
       </div>
     </div>
   </div>
@@ -43,15 +45,18 @@ export default {
 </script>
 
 <style scoped>
-.card{
+.card {
   border: 2px solid #a9a89f;
 }
 
 .center-container {
   display: flex;
-  justify-content: space-around;
+  justify-content: center;
+  flex-direction: row;
   align-items: center;
-  height: 100vh;
+  gap: 100px; /*расстояние между иконкой и инпутом*/
+  width: 1200px;
+  height: 80vh;
 }
 
 .logo-img {
